8c2d7b8a2184231d164bfe251164cf216e22779c
niuyu <inewyu@gmail.com>
2016-09-27 15:58:44 +0800

    add link reverse and quick sort
    Signed-off-by: niuyu <inewyu@gmail.com>

---
+++ src/com/example/algorithm/link/Node.java
@@ -0,0 +1,85 @@
+package com.example.algorithm.link;
+
+/**
+ * Created by niuyu on 7/13/16.
+ */
+public class Node {
+
+    public int value;
+    public Node next;
+
+    public Node() {
+        value = -1;
+        next = null;
+    }
+
+    public Node(int value) {
+        this.value = value;
+        next = null;
+    }
+
+    public void add(Node head, Node add) {
+        if (head == null || add == null) {
+            return;
+        }
+
+        Node p = head;
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = add;
+    }
+
+    public Node reverse() {
+        Node p1 = this;
+        Node p2 = p1.next;
+        Node p3 = p2.next;
+        p1.next = null;
+
+        while (p3 != null) {
+            p2.next = p1;
+            p1 = p2;
+            p2 = p3;
+            p3 = p3.next;
+        }
+
+        p2.next = p1;
+
+        return p2;
+    }
+
+    public void print(Node head) {
+        Node p = head;
+        if (p == null) {
+            System.out.println("NodeList is null");
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        while (p != null) {
+            if (builder.length() > 0) {
+                builder.append("->");
+            }
+            builder.append(p.value);
+            p = p.next;
+        }
+
+        System.out.println(builder.toString());
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(0);
+
+        for (int i = 1; i < 10; i++) {
+            head.add(head, new Node(i));
+        }
+
+        System.out.print("before reverse: ");
+        head.print(head);
+
+        System.out.print("after  reverse: ");
+        head.print(head.reverse());
+    }
+}
---
+++ src/com/example/algorithm/sort/QuickSort.java
@@ -0,0 +1,74 @@
+package com.example.algorithm.sort;
+
+/**
+ * Created by niuyu on 7/3/16.
+ */
+public class QuickSort {
+
+    private static int getMiddle(int[] a, int low, int high) {
+        int tmp = a[low];
+
+        while (low < high) {
+            while (low < high && a[high] >= tmp) {
+                high--;
+            }
+            a[low] = a[high];
+
+            while (low < high && a[low] <= tmp) {
+                low++;
+            }
+            a[high] = a[low];
+        }
+
+        a[low] = tmp;
+
+        return low;
+    }
+
+    private static void quickSort(int[] a, int low, int high) {
+        if (low < high) {
+            int middle = getMiddle(a, low, high);
+            quickSort(a, 0, middle - 1);
+            quickSort(a, middle + 1, high);
+        }
+    }
+
+    private static void quickSort(int[] a) {
+        if (a.length > 0) {
+            quickSort(a, 0, a.length - 1);
+        }
+    }
+
+    private static void print(int[] array) {
+        if (array == null || array.length == 0) {
+            System.out.println("array is null or length is 0");
+            return;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i : array) {
+            if (builder.length() > 0) {
+                builder.append(",");
+            }
+            builder.append(i);
+        }
+
+        System.out.println(builder.toString());
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = {3, 6, 9, 4, 8, 2, 0, 5, 7, 1};
+
+
+        System.out.print("before order: ");
+        print(a);
+
+        quickSort(a);
+
+        System.out.print("after  order: ");
+        print(a);
+    }
+
+}
